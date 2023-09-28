51b9868e55ccefe66f7fde5a73e21c71f9c767b4
Laney Stroup <tstroup@masters.edu>
2023-09-28 03:53:58 +0000

    update employees and devices views to show Assigned Devices/Employee; add full_name method, and use for simple form;

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -53,6 +53,6 @@ class EmployeesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def employee_params
-      params.require(:employee).permit(:first_name, :last_name)
+      params.require(:employee).permit(:first_name, :last_name, :device_ids)
     end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,7 @@ class Employee < ApplicationRecord
   has_many :devices
   has_many :manufacturers, :through => :devices
 
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
