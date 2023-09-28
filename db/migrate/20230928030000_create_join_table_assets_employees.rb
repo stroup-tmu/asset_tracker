fe87d7e542af63c967bc888e953bb050b792f49a
Laney Stroup <tstroup@masters.edu>
2023-09-28 03:25:38 +0000

    remove Employee and Manufacturer models;

--- db/migrate/20230928030000_create_join_table_assets_employees.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableAssetsEmployees < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :assets, :employees do |t|
-      # t.index [:asset_id, :employee_id]
-      # t.index [:employee_id, :asset_id]
-    end
-  end
-end
