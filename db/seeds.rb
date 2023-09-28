5d729d50af78bad03c44bfdf24f11e31f6ee6988
Laney Stroup <tstroup@masters.edu>
2023-09-28 06:24:38 +0000

    populate seeds file;

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.create(
+  [
+    { name: "Desktop" },
+    { name: "Laptop" },
+    { name: "Smart Phone" }
+  ]
+)
+
+Manufacturer.create(
+  [
+    { name: "Apple" },
+    { name: "Dell" },
+    { name: "Lenovo" },
+    { name: "Microsoft" },
+    { name: "Samsung" }
+  ]
+)
+
+2.times do |i|
+  Device.create!(
+    [
+      { name: "iPhone #{i}", manufacturer: Manufacturer.find_by(name: "Apple"), category: Category.find_by(name: "Smart Phone") },
+      { name: "Dell Inspiron #{i}", manufacturer: Manufacturer.find_by(name: "Dell"), category: Category.find_by(name: "Desktop") },
+      { name: "Lenovo Thinkpad #{i}", manufacturer: Manufacturer.find_by(name: "Lenovo"), category: Category.find_by(name: "Laptop") },
+      { name: "Samsung Galaxy #{i}", manufacturer: Manufacturer.find_by(name: "Samsung"), category: Category.find_by(name: "Smart Phone") }
+    ]
+  )
+end
+
+Software.create(
+  [
+    { name: "Microsoft Windows", :license_count => 2 },
+    { name: "Microsoft Office", :license_count => 2 },
+    { name: "Dropbox", :license_count => 10 },
+    { name: "Gmail", :license_count => 11 },
+  ]
+)
+
+Employee.create(
+  [
+    { first_name: "John", last_name: "Doe" },
+    { first_name: "Jill", last_name: "Doe" },
+    { first_name: "James", last_name: "Doe" },
+    { first_name: "Jane", last_name: "Doe" },
+  ]
+)
+
+2.times do
+  Employee.all.each do |e|
+    if Device.where(employee: nil).present?
+      e.devices << Device.where(employee: nil)[rand(0..(Device.where(employee: nil).count - 1))]
+    end
+    e.softwares << Software.all[rand(0..(Software.all.count - 1))]
+  end
+end
