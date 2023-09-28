cb04ac0bde92a7a177aa7d7bdc9f74421f5f7006
Laney Stroup <tstroup@masters.edu>
2023-09-28 07:43:09 +0000

    add profile_picture to employees;

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -53,6 +53,6 @@ class EmployeesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def employee_params
-      params.require(:employee).permit(:first_name, :last_name, :device_ids)
+      params.require(:employee).permit(:first_name, :last_name, :device_ids, :profile_picture)
     end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,10 @@ class Employee < ApplicationRecord
   has_many :manufacturers, :through => :devices
   has_and_belongs_to_many :softwares
 
+  has_one_attached :profile_picture do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
   def full_name
     [first_name, last_name].join(" ")
   end
