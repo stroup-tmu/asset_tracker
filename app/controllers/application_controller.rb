1cb6cd3dd6c7ce7eadd7fb24e854ece0b433937a
Laney Stroup <tstroup@masters.edu>
2023-10-07 13:50:56 +0000

    change password to password;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "admin", password: "gRdHJt4c"
+  http_basic_authenticate_with name: "admin", password: "password"
 end
